50c350d1e7de7e858bf2ad941ace01cb19e8b6e9
Miguel Augusto <miguelazuoss@gmail.com>
2023-12-28 03:21:44 -0300

    Try Deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
